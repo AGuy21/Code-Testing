d3da82bd6db1674b96184b443499e3427cde2817
A_Guy <sharkyboy4231@gmail.com>
2024-09-29 13:21:50 -0400

    Fixed bug where it didnt show home page when website first loaded

--- CSNHS/src/App.tsx
+++ CSNHS/src/App.tsx
@@ -1,25 +1,23 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Blog from "./pages/Blog";
 
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
-import './index.css'
-import Home from './pages/Home'
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Blog from './pages/Blog';
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/home' Component={Home} />
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/home" Component={Home} />
         <Route path="/about" Component={About} />
         <Route path="/projects" Component={Projects} />
         <Route path="/blog" Component={Blog} />
         {/* Add a fallback route or a 404 page if needed */}
-        </Routes>
+      </Routes>
     </BrowserRouter>
   );
 };
 
-
-export default App
-
-
+export default App;
