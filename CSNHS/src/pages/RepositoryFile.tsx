b8c6e7431d09b325bbe4721ed4f755841d14a642
A_Guy <sharkyboy4231@gmail.com>
2024-10-05 15:42:43 -0400

    Added code formatting for files

--- CSNHS/src/pages/RepositoryFile.tsx
+++ CSNHS/src/pages/RepositoryFile.tsx
@@ -1,9 +1,29 @@
 import { useParams } from "react-router-dom";
 import { GetRepoFileContent } from "../backend/GetRepoFileContent";
+import { RepoFile } from "../types/RepoFile";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { solarizedDarkAtom  } from "react-syntax-highlighter/dist/esm/styles/prism"; // Change the style here
 
-
+const ErrorContent: RepoFile = {
+  name: "Error",
+  path: "",
+  sha: "",
+  size: 999,
+  url: "",
+  html_url: "",
+  git_url: "",
+  download_url: null,
+  type: "file",
+  content:
+    "RXJyb3IgZmluZGluZyB0aGlzIGZpbGUncyBpbmZvcm1hdGlvbi4gUGxlYXNlIHJlbG9hZCBvciByZXBvciBwcm9ibGVtLiBUaGVyZSBtYXkgYWxzbyBiZSBhIGNoYW5jZSB0aGlzIGZpbGUgaGFzIG5vIGNvZGUu",
+  encoding: "base64",
+  _links: {
+    self: "",
+    git: "",
+    html: "",
+  },
+};
 const RepositoryFile = () => {
-  
   const { "*": repoItemPath } = useParams();
   const actualFilePath = repoItemPath?.replace("/code", ""); // Correctly remove '/code'
 
@@ -12,15 +32,27 @@ const RepositoryFile = () => {
   }
 
   // Pass the actual file path to GetRepoFileCode
-  const fileContent = GetRepoFileContent(actualFilePath); 
-
+  const fileContent = GetRepoFileContent(actualFilePath) || ErrorContent;
 
   return (
     <div>
-      <h1>Viewing File: {actualFilePath}</h1>
-      <pre>{JSON.stringify(fileContent, null, 2)}</pre> {/* Display the code */}
+      <SyntaxHighlighter language="javascript" style={solarizedDarkAtom  }>
+        {atob(fileContent.content)}
+      </SyntaxHighlighter>
     </div>
   );
 };
+/**
+ * 
 
+const CodeBlock = ({ codeString }) => {
+  return (
+    <div style={{ padding: "20px", backgroundColor: "#f5f5f5", borderRadius: "5px" }}>
+      <SyntaxHighlighter language="javascript" style={solarizedlight}>
+        {codeString}
+      </SyntaxHighlighter>
+    </div>
+  );
+};
+ */
 export default RepositoryFile;
