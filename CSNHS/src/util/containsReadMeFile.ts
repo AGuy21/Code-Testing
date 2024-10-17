b702d7cb6f697f8eed61f2544001f9c0bdf4f088
A_Guy <sharkyboy4231@gmail.com>
2024-10-17 16:51:41 -0400

    Fixed containsReadMeFile function as the foreach was iterating while it returns false, .some method fixed it

--- CSNHS/src/util/containsReadMeFile.ts
+++ CSNHS/src/util/containsReadMeFile.ts
@@ -1,18 +1,12 @@
 /**
  * - This is a utility function used to take in a repository folder and return true if it has a README file if not returns false
  *
- * @param RepoFolder = The repository folder to be checked for README in the function
- * @returns {boolean} - true if has README file false otherwise
+ * @param RepoFolder - The repository folder to be checked for README in the function
+ * @returns {boolean} - true if has README file, false otherwise
  */
 
 import { RepoFolder } from "../types/RepoFolder";
 
 export function containsReadMeFile(RepoFolder: RepoFolder[]) {
-  RepoFolder.forEach((item) => {
-    console.log(item.name + ' === About.md' )
-    if (item.name === "About.md") {
-      return true;
-    }
-  });
-  return false;
+  return RepoFolder.some((item) => item.name === "About.md");
 }
