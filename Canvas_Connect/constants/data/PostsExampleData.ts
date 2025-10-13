93fb2831f52c49e48c3f907c56e1c213cca95467
Jaxon Perez <Sharkyboy4231@gmail.com>
2025-10-12 20:48:03 -0400

    CHORE: cleanup post's example data with new su b-folder in constants folder

---
+++ Canvas_Connect/constants/data/PostsExampleData.ts
@@ -0,0 +1,32 @@
+export const exampleData = [
+    {
+      title: "Hello",
+      description:
+        "i made this too test the rendering of the component, this data is titled Hello.",
+      picture:
+        "file:///data/user/0/host.exp.exponent/cache/ExperienceData/%2540anonymous%252FCanvas_Connect-311391b7-2ccc-4871-90f8-f2752143f633/ImagePicker/18407126-e943-4298-886b-cf8ef7c93415.jpeg",
+      likes: 735,
+      comments: 65,
+      postID: 1,
+    },
+    {
+      title: "Hello2",
+      description:
+        "i made this too test the rendering of the component, this data is titled Hello2.",
+      picture:
+        "file:///data/user/0/host.exp.exponent/cache/ExperienceData/%2540anonymous%252FCanvas_Connect-311391b7-2ccc-4871-90f8-f2752143f633/ImagePicker/18407126-e943-4298-886b-cf8ef7c93415.jpeg",
+      likes: 24,
+      comments: 6,
+      postID: 2,
+    },
+    {
+      title: "Hello3",
+      description:
+        "i made this too test the rendering of the component, this data is titled Hello3.",
+      picture:
+        "file:///data/user/0/host.exp.exponent/cache/ExperienceData/%2540anonymous%252FCanvas_Connect-311391b7-2ccc-4871-90f8-f2752143f633/ImagePicker/18407126-e943-4298-886b-cf8ef7c93415.jpeg",
+      likes: 858977,
+      comments: 84770,
+      postID: 3,
+    },
+  ];
