16d5c63c7a660868d671a98523f5c736a78d0d18
Jaxon Perez <Sharkyboy4231@gmail.com>
2025-10-25 15:25:39 -0400

    FEAT: make previous posts update after new post madae

--- Canvas_Connect/components/ui/PreviousPosts.tsx
+++ Canvas_Connect/components/ui/PreviousPosts.tsx
@@ -1,5 +1,5 @@
 import { FlatList, StyleSheet, Text, View, Image } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { LinearGradient } from "expo-linear-gradient";
 import {
   heightPercentageToDP as hp,
@@ -8,14 +8,34 @@ import {
 import Colors from "@/constants/Colors";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import AntDesign from "@expo/vector-icons/AntDesign";
-import { exampleData } from "@/constants/data/PostsExampleData";
 import { useUserDataStore } from "../hooks/store";
 import GetUsersPosts from "../functions/GetUsersPosts";
+import { postType } from "@/constants/types/postType";
+import { useFocusEffect } from "expo-router";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "@/Configs/FirebaseConfig";
 
 const PreviousPosts = () => {
   const userData = useUserDataStore((state) => state.data);
+  const setUserData = useUserDataStore((state) => state.setData);
 
-  const userPosts = GetUsersPosts()
+  // Refresh user data when the user comes back to screen, this is so that the posts list updates after creating a new post
+  useFocusEffect(
+    React.useCallback(() => {
+      const refreshUserData = async () => {
+        if (userData?.email) {
+          const userDoc = await getDoc(doc(db, "users", userData.email));
+          if (userDoc.exists()) {
+            setUserData(userDoc.data() as any);
+          }
+        }
+      };
+      refreshUserData();
+    }, [userData?.email])
+  );
+
+  const userPosts = GetUsersPosts();
+
   return (
     <FlatList
       style={styles.previousPostsLists}
@@ -30,7 +50,10 @@ const PreviousPosts = () => {
             style={styles.imageGradient}
           />
 
-          <Image source={{uri: userData.profilePicture}} style={styles.postPicture} />
+          <Image
+            source={{ uri: userData.profilePicture }}
+            style={styles.postPicture}
+          />
 
           <View style={styles.postStats}>
             <View
@@ -39,7 +62,11 @@ const PreviousPosts = () => {
                 gap: wp(1),
               }}
             >
-              <FontAwesome name="comments" size={wp(6)} color={Colors.background} />
+              <FontAwesome
+                name="comments"
+                size={wp(6)}
+                color={Colors.background}
+              />
               <Text style={styles.postStatText}>{24}</Text>
             </View>
             <View
