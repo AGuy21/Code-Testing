918722bf18febb1843616ff2412a53d3796d61f2
Jaxon Perez <Sharkyboy4231@gmail.com>
2025-11-29 23:37:10 -0500

    FEAT: Astronomy club sponsorship with firebase config

--- Astronomy/FirebaseConfig.ts
+++ Astronomy/FirebaseConfig.ts
@@ -1,11 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDs3eDy-Hdv0w6o_1Yz6hddRgRuJ2QClGg",
   authDomain: "nease-astronomy.firebaseapp.com",
@@ -19,3 +16,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { app, analytics, db };
