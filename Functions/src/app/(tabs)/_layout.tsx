95a45d47a67ddd3b25cf481c4810c7e89a30f007
Jaxon Maxwell Perez <jaxonp144@gmail.com>
2026-08-12 16:26:16 -0400

    FIX: fix needs commit

--- Functions/src/app/(tabs)/_layout.tsx
+++ Functions/src/app/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-import { Ionicons } from "@expo/vector-icons";
+import Ionicons from "@react-native-vector-icons/ionicons";
 import { Tabs } from "expo-router";
 import { Platform, TouchableOpacity } from "react-native";
 import { useColorTheme } from "../../hooks/useColorTheme";
