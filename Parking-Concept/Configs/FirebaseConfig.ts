8b37fd84e6e45034ef8491d8fe85db84b2cdc82c
Jaxon Maxwell Perez <jaxonp144@gmail.com>
2026-08-25 16:29:21 -0400

    ADD: new config changes and starting layout

---
+++ Parking-Concept/Configs/FirebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBnGiRrZffYxK_fArTXwaY-ivsAL-2ci0c",
+  authDomain: "parking-concept.firebaseapp.com",
+  projectId: "parking-concept",
+  storageBucket: "parking-concept.firebasestorage.app",
+  messagingSenderId: "436147832210",
+  appId: "1:436147832210:web:5dbf24986a375e4cc76d4b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
