76cacf9e626ea1a3457bb6081baad35d019878a1
Jaxon Maxwell Perez <jaxonp144@gmail.com>
2026-09-08 13:45:07 -0400

    ADD: ending parking functionality added

--- Parking-Concept/componenets/functions/removeCar.ts
+++ Parking-Concept/componenets/functions/removeCar.ts
@@ -7,9 +7,11 @@ import {
   serverTimestamp,
   getDoc,
   doc,
+  deleteDoc,
 } from "firebase/firestore";
 import getDocId from "./getDocId";
 import getTimeTotal from "./getTimeTotal";
+import { Alert } from "react-native";
 
 const db = getFirestore();
 
@@ -20,19 +22,23 @@ export default async function removeCar(
 ) {
   try {
     const docId = getDocId(lot, plate, password);
-    const docRef = doc(db, "Lots", lot, "Cars", docId );
+    const docRef = doc(db, "Lots", lot, "Cars", docId);
 
     const docSnap = await getDoc(docRef);
     const docData = docSnap.data() as Car;
 
     if (docData.Password == password) {
-        console.log("Authentication successful for:", plate)
-        const elapsedTime = getTimeTotal(docData)
-        
+      console.log("Authentication successful for:", plate);
+      const elapsedTime = await getTimeTotal(docData);
+      const timeString = elapsedTime?.toString();
+      console.log("Deleting Doc...");
+      deleteDoc(docRef);
+      Alert.alert("Billed for total time: ", timeString) + " Minutes";
+      router.replace("/");
     } else {
-        console.log("Authentication failed for:", plate)
+      console.log("Authentication failed for:", plate);
+      Alert.alert("Authentication Failed");
     }
-
   } catch (error) {
     console.error("Error adding document: ", error);
   }
